97d3099dea81c8b7a4e8351f253e96da8ef23658
AlekzNet <alekz.net@gmail.com>
2017-02-24 01:14:31 +0100

    Create fgt-set.sh

---
+++ fgt-set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FWS=fgt.list
+USERNAME=admin
+PASSWD='my-password'
+
+#echo -n "Enter the username: "
+#read -e  USERNAME
+#echo -n "Enter the SSH password: "
+#read -s -e PASSWD
+#echo -ne '\n'
+
+while read fw
+do
+        set $fw
+        mkdir -p $2
+        OUTFILE=$2/$2.out
+        ./fgt-set.exp $1 $2 $USERNAME $PASSWD $OUTFILE
+done < $FWS
