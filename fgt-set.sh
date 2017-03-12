18261e21aa4e3358f711922294344c3b3082337d
AlekzNet <alekz.net@gmail.com>
2017-03-12 18:38:29 +0100

    Comments support

--- fgt-set.sh
+++ fgt-set.sh
@@ -10,10 +10,10 @@ PASSWD='my-password'
 #read -s -e PASSWD
 #echo -ne '\n'
 
-while read fw
+grep -v '^[     ]*#' $FWS | while read fw
 do
         set $fw
         mkdir -p $2
         OUTFILE=$2/$2.out
         ./fgt-set.exp $1 $2 $USERNAME $PASSWD $OUTFILE
-done < $FWS
+done
